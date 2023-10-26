57b25a7e983830d472eb88459e10cc8dc4129149
Manuel Sánchez Suyón <manueduardo000@hotmail.com>
2023-10-25 22:03:25 -0500

    configuración base

---
+++ src/components/ProtectedRouter.tsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom"
+
+function ProtectedRoute() {
+  const  isAuthenticated  = true;
+  if(!isAuthenticated) {
+    return <Navigate to="/login" replace />
+  }
+  return <Outlet />
+}
+
+export default ProtectedRoute;
