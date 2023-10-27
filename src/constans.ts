4178e167413962b04287e7823c312ce8d366e040
Manuel Sánchez Suyón <manueduardo000@hotmail.com>
2023-10-27 11:32:13 -0500

    diary services

--- src/constans.ts
+++ src/constans.ts
@@ -6,3 +6,5 @@ export const ROUTES = {
     breathing: "/respiracion",
     home: "/home",
 }
+
+export const api_url_base = "http://172.174.193.122/api/"
--- src/models.ts
+++ src/models.ts
@@ -1,11 +1,34 @@
-export interface User{
-    name: string;
-    days_on_app : number;
-    phone_number:string;
+export interface User {
+  id: number;
+  name: string;
+  days_on_app: number;
+  phone_number: string;
 }
 
-export const EmptyUser: User = {
-    name: "Lucia",
-    days_on_app: 2,
-    phone_number: "",
+export interface Note {
+  id: number;
+  title: string;
+  text: string;
+  diary_id: number;
 }
+
+export interface NewNote {
+  id: number;
+  user_id: number;
+  note_title: string;
+  note_text: string;
+}
+
+export const EmptyNote: Note = {
+  id: 2,
+  title: "Me cai de la cama",
+  text: "Casito, pero no.",
+  diary_id: 1,
+};
+
+export const EmptyUser: User = {
+  id: 1,
+  name: "Manuel",
+  days_on_app: 2,
+  phone_number: "",
+};
